438f4818d0f4793c061af98cee64210821f1ef53
Koru <flyingshadow03@gmail.com>
2024-09-18 19:39:55 +0530

    Updated database

--- run.sh
+++ run.sh
@@ -7,13 +7,12 @@ set -e
 # find . -type d \( -name env -o -name venv  \) -prune -false -o -name "*.pyc" -exec rm -rf {} \;
 
 # Run required migrations
-export FLASK_APP=core/server.py
-rm core/store.sqlite3
-flask db upgrade -d core/migrations/
+export FLASK_APP=core.server.py
+# rm core/store.sqlite3
 
 # flask db init -d core/migrations/
-# flask db migrate -m "Initial migration." -d core/migrations/
 # flask db upgrade -d core/migrations/
+# flask db migrate -m "Initial migration." -d core/migrations/
 
 # Run server
 gunicorn -c gunicorn_config.py core.server:app
